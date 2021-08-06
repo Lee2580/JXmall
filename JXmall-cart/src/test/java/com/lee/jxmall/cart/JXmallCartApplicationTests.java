b9a74e246e758a8b317f1e7da3913068e7561f19
lee <1114862851@qq.com>
2021-08-06 15:50:00 +0800

    商城业务-购物车-环境搭建

---
+++ JXmall-cart/src/test/java/com/lee/jxmall/cart/JXmallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.jxmall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JXmallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
